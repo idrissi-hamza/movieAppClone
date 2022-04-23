71fc22006eb31d80dfbb948267e31d4adf095cc9
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-04-23 21:58:28 +0200

    finish thumbnail display

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,29 @@
-import React from 'react'
-const Thumbnail = ({result}) => {
-  return (
-    <pre>
-      {JSON.stringify(result,null,2)}
-     
-    </pre>
-  )
-}
+import React from "react";
+import Image from "next/image";
+import { ThumbUpIcon } from "@heroicons/react/outline";
 
-export default Thumbnail 
+const Thumbnail = ({ result }) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  return (
+    <pre className="p-2 group cursor-pointer transition duration-200  transform sm:hover:scale-105 ">
+      {/* {JSON.stringify(result, null, 2)} */}
+      <Image
+        layout="responsive"
+        src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
+        width={1920}
+        height={1080}
+      />
+      <div className="p-2 ">
+        <p className="truncate max-w-md">{result.overview}</p>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100 ">
+          {result.media_type && `${result.media_type}`}{" "}
+          {result.release_date || result.first_air_date}
+          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+        </p>
+      </div>
+    </pre>
+  );
+};
+
+export default Thumbnail;
